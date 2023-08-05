862540e93743ae875f9c4f4fa16cadb093e1daba
Antti Pitkänen <anttis.pitkanen@gmail.com>
2023-08-05 13:10:55 +0300

    fix parsing tests

--- typescript/src/common/parser.test.ts
+++ typescript/src/common/parser.test.ts
@@ -1,5 +1,8 @@
 import { describe, expect, it } from 'vitest';
 import { parseRow } from './parser';
+import { DEFAULT_NUMBER_OF_COLORS, getAvailableColors } from './rules';
+
+const availableColors = getAvailableColors(DEFAULT_NUMBER_OF_COLORS);
 
 describe('parseRow', () => {
   it.each([
@@ -10,7 +13,7 @@ describe('parseRow', () => {
     'red,green,blue,red, yellow', // duplicate red
     'red,green,blue,yellow,black', // too many colors
   ])('rejects invalid input (%s)', (invalidRow) => {
-    const result = parseRow(invalidRow);
+    const result = parseRow(availableColors)(invalidRow);
     expect(result.valid).toBe(false);
   });
 
@@ -19,7 +22,7 @@ describe('parseRow', () => {
     'white, black, brown, orange',
     ' white,    black   , BROWN,  oRanGE',
   ])('accepts valid input (%s)', (validRow) => {
-    const result = parseRow(validRow);
+    const result = parseRow(availableColors)(validRow);
     expect(result.valid).toBe(true);
   });
 });
